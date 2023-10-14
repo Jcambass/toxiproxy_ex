27fe3acb72350ed6ea5ae0febf3234f6ccbb1040
Andrea Leopardi <an.leopardi@gmail.com>
2023-10-14 10:56:58 +0200

    Bump :dialyxir requirement

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule ToxiproxyEx.MixProject do
       {:castore, "~> 1.0.3"},
       {:mint, "~> 1.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
     ]
   end
 
