0994122028bef04da5716b3b2a3e70eeb7acdf01
Joel Ambass <Jcambass@users.noreply.github.com>
2025-07-29 10:42:03 +0000

    Release version 2.0.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ToxiproxyEx.MixProject do
   def project do
     [
       app: :toxiproxy_ex,
-      version: "1.1.1",
+      version: "2.0.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
